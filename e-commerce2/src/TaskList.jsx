79a4c1663cb35966ffe5323e5a3d6a8ad2f864d3
rich <rickygb082002@gmail.com>
2025-08-30 21:24:28 -0600

    commit2

--- e-commerce2/src/TaskList.jsx
+++ e-commerce2/src/TaskList.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
+import { tasks as data} from "./task.js";
 
 export function TaskList() {
-  return (
-    <div>
-        TaskList
-    </div>
-  )
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        setTasks(data);
+    }, []);
+
+    if (tasks.length === 0) {
+        return <h1>No hay Tareas aun</h1>
+    }
+    return (
+        <div>
+            {tasks.map((task) => (
+                <div>
+                    <h1> {task.title} </h1>
+                    <p> {task.description} </p>
+                </div>
+            ))}
+        </div>
+    );
 }
 
-export default TaskList
+export default TaskList;
